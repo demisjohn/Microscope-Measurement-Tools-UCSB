08668ad6add093473413a2bde2481118d3b61b41
demis <demis.john@gmail.com>
2019-05-30 10:52:30 -0700

    Inital commit with UCSB Miroscope Calibrations

--- Analyze/Microscope Measurement Tools/Microscope_Calibrations_user_settings.py
+++ Analyze/Microscope Measurement Tools/Microscope_Calibrations_user_settings.py
@@ -1,23 +1,14 @@
 '''
-	FIJI Plugin - Microscope Measurement Tools
-        Configuration for Choose_Microscope_Calibration.py & Draw_Measurement_-_Line.py
-        Demis D. John, University of California Santa Barbara, Nanofabrication Facility, 2019
-        https://www.nanotech.ucsb.edu
+	FIJI Plugin
+Configuration for Microscope Calibrations
+Demis D. John, Univ. of California Santa Barbara, 2019
 
-Make sure the lists `names`, `cals`, `units` and `aspect_ratio` all have the same number of items!
+This version of the settings file contains a pointer to a custom function, which automatically sets the scale by loading a text file that corresponds to the loaded image file.  The custom function is found in a separate file "JEOL_SEM_AutoCal.py", and you can see the class defined in this file being loaded below.
+In addition, an instance of the class is inserted into the lists of names/calibrations, and all information will be obtained from the Class/external file.
+
+Please make sure the lists `names`, `cals` and `units` all have the same number of items!
 
 After you edit this file, be sure to delete the corresponding *.pyclass file and restart Fiji for the settings to take effect.
-
-
-All of these lists are regular Python Lists, so define them however you prefer to define pythonic lists.  You can define them in-line like normal:
-    x = [ 'one', 'two',  'three' ]
-or in a vertical fashion:
-    x = [
-            'one',      # you can even comment on each line of the list
-            'two',
-            'three'
-        ]
-or even do python math on the lists, such as repeating an item with `*`, or using "list comprehensions".
 '''
 
 
@@ -29,41 +20,176 @@ or even do python math on the lists, such as repeating an item with `*`, or usin
 Microscope scaling/pixel-size calibration settings.
 """
 
-# The names of the microscope calibrations (shows up as the radio button/drop-down names):
+# The names of the microscope calibrations (shows up as the radio button names):
+#   For custom functions, pass the handle to an instantiated class instead of a string.
+
+# Custom Function: Load JEOL SEM autocal class from a file:
+from JEOL_SEM_AutoCal   import JEOL_SEM_CalFromTxt
+jeol_sem_cal_from_txt = JEOL_SEM_CalFromTxt()    # instantiate the class
+
+
+# This defines the items in the drop-down list:
 names = [
-        'FluoroScope 5x', 
-        'FluoroScope 20x', 
-        'FluoroScope 50x', 
-        'FluoroScope 100x', 
-        'FluoroScope 150x',
-        'Olympus DUV 100x',
+        'FluoroScope (Olympus MX51): 5x', 
+        'FluoroScope (Olympus MX51): 20x', 
+        'FluoroScope (Olympus MX51): 50x', 
+        'FluoroScope (Olympus MX51): 100x', 
+        'FluoroScope (Olympus MX51): 150x',
+        
+        'Microscope #2 (Nikon Optiphot 200): 5x',
+        'Microscope #2 (Nikon Optiphot 200): 10x',
+        'Microscope #2 (Nikon Optiphot 200): 50x',
+        'Microscope #2 (Nikon Optiphot 200): 100x',
+        'Microscope #2 (Nikon Optiphot 200): 150x',
+        
+        'Microscope #3 (Nikon Eclipse L200): 5x',
+        'Microscope #3 (Nikon Eclipse L200): 10x',
+        'Microscope #3 (Nikon Eclipse L200): 20x',
+        'Microscope #3 (Nikon Eclipse L200): 50x',
+        'Microscope #3 (Nikon Eclipse L200): 100x',
+        'Microscope #3 (Nikon Eclipse L200): 150x',
+        
+        'Microscope #4 (Nikon Eclipse L200D): 5x',
+        'Microscope #4 (Nikon Eclipse L200D): 10x',
+        'Microscope #4 (Nikon Eclipse L200D): 20x',
+        'Microscope #4 (Nikon Eclipse L200D): 50x',
+        'Microscope #4 (Nikon Eclipse L200D): 100x',
+        'Microscope #4 (Nikon Eclipse L200D): 150x',
+        
+        'Olympus DUV (MX61A): 5x',
+        'Olympus DUV (MX61A): 10x',
+        'Olympus DUV (MX61A): 20x',
+        'Olympus DUV (MX61A): 50x',
+        'Olympus DUV (MX61A): 100x',
+        'Olympus DUV (MX61A): DUV 100x',
+        
+        jeol_sem_cal_from_txt,   # instance of class with custom function `MyClass.cal()` for setting image scale.
+        
         ]
 
 
-# The 'pixel-per-unit' obtained from the "Set Scale..." dialog, for each named calibration above:
+# The 'pixel-per-unit' obtained from the "Set Scale..." Dialog, for each named calibration above:
 cals = [
-        0.9058,     # FluoroScope 5x to 150x
+        0.9058,     # FluoroScope (Olympus MX51)
         1.81,  
         4.525,  
         9.0667,  
         13.5333,
-        54.6875,    # Olympus DUV
+        
+        0.956064,    # Microscope #2 (Nikon)
+        1.9149,
+        9.6137,
+        19.2786,
+        28.862,
+        
+        1.020553,   # Microscope #3 (Nikon)
+        2.032475,
+        4.084,
+        10.171,
+        20.354,
+        30.696,
+        
+        0.7464374,   # Microscope #4 (Nikon)
+        1.497718,
+        2.999,
+        7.350,
+        14.984,
+        22.536,
+        
+        0.6900,     # Olympus DUV (MX61A)
+        1.3903,
+        2.7800,
+        6.9200,
+        13.9000,
+        54.6875,
+        
+        jeol_sem_cal_from_txt,  # placeholder for custom function
         ]
 #   This is just 1/pixel_width, in case you were wondering.
 
 
 # length-units for each calibration:
-#units = ['um','um','um','um','um']
-# for identical units for all cals, can use the following line instead (uncomment):
-units = ['um'    for x in cals]    # list-comprehension with constant `um`
+units = [
+        'um',       # FluoroScope (Olympus MX51)
+        'um',
+        'um',
+        'um',
+        'um',
+        
+        'um',       # Microscope #2 (Nikon): 5x
+        'um',
+        'um',
+        'um',
+        'um',
+        
+        'um',       # Microscope #3 (Nikon)
+        'um',
+        'um',
+        'um',
+        'um',
+        'um',
+        
+        'um',       # Microscope #4 (Nikon)
+        'um',
+        'um',
+        'um',
+        'um',
+        'um',
+        
+        'um',       # Olympus DUV (MX61A)
+        'um',
+        'um',
+        'um',
+        'um',
+        'um',
+        
+        jeol_sem_cal_from_txt,
+        ]
+# for identical units for all cals, use the following line instead (uncomment):
+#units = ['um'    for x in cals]    # list-comprehension with constant `um`
 
 
 # Aspect Ratio for each calibration (default should be 1.0 )
 #   Ratio is defined as so: pixelHeight = pixelWidth * AspectRatio
-# The following sets the aspect ratio to 1 for all calibrations:
-aspect_ratio =  [ 1.0    for x in cals ]   # list-comprehension with constant `1`
 # Uncomment the following line to use custom aspect ratios:
-#aspect_ratio = [1, 1, 1, 1, 1]
+aspect_ratio = [
+        1,      # FluoroScope (Olympus MX51)
+        1, 
+        1, 
+        1, 
+        1,
+        
+        1,      # Microscope #2 (Nikon): 5x
+        1, 
+        1, 
+        1, 
+        1,
+        
+        1,      # Microscope #3 (Nikon)
+        1,
+        1,
+        1,
+        1,
+        1,
+        
+        1,      # Microscope #4 (Nikon)
+        1,
+        1,
+        1,
+        1,
+        1,
+        
+        1,      # Olympus DUV (MX61A)
+        1,
+        1,
+        1,
+        1,
+        1,
+        
+        jeol_sem_cal_from_txt,
+        ]
+# The following sets the aspect ratio to 1 for all calibrations (uncomment to use):
+#aspect_ratio =  [ 1.0    for x in cals ]   # list-comprehension with constant `1`
 
 
 
@@ -104,7 +230,7 @@ texttoleft = True      # put text on left or right side of last point?
 
 '''
 ---------------------------------------------------------
-Warn user if they erroneously run this file as a stand-alone plugin.
+Warn user if they run this file as a stand-alone plugin.
 '''
 
 def run():
